7c895a60ae413d460f2972f2d2684c71c99f92f7
PolGames <company.polgames@mail.ru>
2026-01-24 19:35:52 +0600

    Add and Remove listener

--- Assets/SpinManager.cs
+++ Assets/SpinManager.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpinManager : MonoBehaviour
 {
     private Animator AnimatorPopAppGoTGFirstSpin;
     [SerializeField] private GameObject PopAppGoTGFirstSpin;
+    [SerializeField] private Button ButtonSpin;
 
     private void Start()
     {
         AnimatorPopAppGoTGFirstSpin = PopAppGoTGFirstSpin.GetComponent<Animator>();
+        ButtonSpin.onClick.AddListener(GoTGFirstSpin); //Добавляем событие на кнопку крутить, чтобы появился поп-апп
     }
 
     public void GoTGFirstSpin()
@@ -18,5 +21,7 @@ public class SpinManager : MonoBehaviour
     public void ClickSB()
     {
         Application.OpenURL("https://www.google.com/?hl=ru");
+        PopAppGoTGFirstSpin.SetActive(false);
+        ButtonSpin.onClick.RemoveListener(GoTGFirstSpin); //Убираем событие с появлением пап-аппа, чтобы была возможность использовать другие меоды
     }
 }
